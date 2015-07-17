f79a0489929dd1b7744e3b17a66b95a7fcbd6bb7
Tommy Boucher <boucher@cs.umass.edu>
2015-07-17 15:06:33 -0400

    Added swiss roll demo.

--- ller.py
+++ ller.py
@@ -124,3 +124,33 @@ class LLER(LocallyLinearEmbedding):
             mu=self.mu, gamma=self.gamma)
         return self
 
+
+def demo(k=7):
+    from ller import LLER
+    from mpl_toolkits.mplot3d import Axes3D
+    from sklearn.datasets import make_swiss_roll
+    import matplotlib.pyplot as plt
+
+    X,t = make_swiss_roll()
+
+    lle = LocallyLinearEmbedding(n_components=2,n_neighbors=k)
+    lle_X = lle.fit_transform(X)
+
+    ller = LLER(n_components=2,n_neighbors=k)
+    ller_X = ller.fit_transform(X,t)
+
+    fig = plt.figure(figsize=plt.figaspect(0.33))
+    ax = fig.add_subplot(1, 3, 1, projection='3d')
+    ax.scatter(X[:,0],X[:,1],X[:,2],c=t,s=50)
+    ax.set_title('Swiss Roll')
+    ax = fig.add_subplot(1, 3, 2)
+    ax.scatter(lle_X[:,0],lle_X[:,1],c=t,s=50)
+    ax.set_title('LLE Embedding')
+    ax = fig.add_subplot(1, 3, 3)
+    ax.scatter(ller_X[:,0],ller_X[:,1],c=t,s=50)
+    ax.set_title('LLER Embedding')
+    plt.show()
+
+
+if __name__ == "__main__":
+    demo()
